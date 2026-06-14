fd330ce5961088a4429c159de8c80e339306f2ce
Kadir YILMAZ <kadiryilmaz19821@gmail.com>
2026-06-15 01:01:24 +0300

    d6

--- GameGaraj.PhotoStock.API/Models/PhotoLimits.cs
+++ GameGaraj.PhotoStock.API/Models/PhotoLimits.cs
@@ -1,6 +1,11 @@
 ﻿namespace GameGaraj.PhotoStock.API.Models
 {
-    public class PhotoLimits
+    public static class PhotoLimits
     {
+        public const int MaxPhotos = 5;
+        public const long MaxFileSize = 5 * 1024 * 1024;
+
+        public static readonly string[] AllowedExtensions =
+            { ".jpg", ".jpeg", ".png", ".webp" };
     }
 }
