fc6d6e24825f2625b9b6324c5af2853f8cf938b3
Kadir YILMAZ <kadiryilmaz19821@gmail.com>
2026-07-02 16:58:45 +0300

    es log bug fix

--- GameGaraj.Shared/Logging/SerilogConfiguration.cs
+++ GameGaraj.Shared/Logging/SerilogConfiguration.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -12,8 +14,32 @@ namespace GameGaraj.Shared.Logging
     {
         public static void AddSerilogLogging(this WebApplicationBuilder builder, string serviceName)
         {
+            // Enable Serilog SelfLog to help diagnose Elasticsearch/File sink issues
+            try
+            {
+                var logDirectory = "../ConsoleLogs";
+                if (!Directory.Exists(logDirectory))
+                {
+                    Directory.CreateDirectory(logDirectory);
+                }
+                var selfLogPath = Path.Combine(logDirectory, "serilog-selflog.txt");
+                Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(new StreamWriter(selfLogPath, true)));
+            }
+            catch
+            {
+                Serilog.Debugging.SelfLog.Enable(Console.Error);
+            }
+
             var environment = builder.Environment.EnvironmentName;
-            var elasticUri = builder.Configuration["ElasticSearchSettings:Uri"];
+            var elasticUri = builder.Configuration["ElasticSearchSettings:Uri"]
+                             ?? builder.Configuration["ObservabilitySettings:ElasticSearchUri"];
+
+            // Fallback to localhost:9201 in development if not configured
+            if (string.IsNullOrEmpty(elasticUri) && builder.Environment.IsDevelopment())
+            {
+                elasticUri = "http://localhost:9201";
+            }
+
             var serviceSlug = serviceName.ToLowerInvariant().Replace(".", "-");
             var environmentSlug = environment.ToLowerInvariant();
 
@@ -34,10 +60,7 @@ namespace GameGaraj.Shared.Logging
             if (!string.IsNullOrEmpty(elasticUri))
             {
                 loggerConfig.WriteTo.Logger(logs => logs
-                    .Filter.ByIncludingOnly(evt =>
-                        evt.Properties.TryGetValue("LogType", out var logType) &&
-                        logType is ScalarValue sv &&
-                        sv.Value?.ToString() == "HttpRequest")
+                    .Filter.ByIncludingOnly(IsHttpRequestLog)
                     .Enrich.With<RequestLogPropertyPruner>()
                     .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
                     {
@@ -50,6 +73,20 @@ namespace GameGaraj.Shared.Logging
 
             Log.Logger = loggerConfig.CreateLogger();
             builder.Host.UseSerilog();
+        }
+
+        private static bool IsHttpRequestLog(LogEvent evt)
+        {
+            if (evt.Properties.TryGetValue("LogType", out var logType) &&
+                logType is ScalarValue logTypeValue &&
+                logTypeValue.Value?.ToString() == "HttpRequest")
+            {
+                return true;
+            }
+
+            return evt.Properties.TryGetValue("SourceContext", out var sourceContext) &&
+                   sourceContext is ScalarValue sourceContextValue &&
+                   sourceContextValue.Value?.ToString() == "Serilog.AspNetCore.RequestLoggingMiddleware";
         }
     }
 }
